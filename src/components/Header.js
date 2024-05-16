62eae9f3cc5ecd0c6cbece169ae8be347b6ce521
Wais000 <waisrbt1@gmail.com>
2024-05-16 11:27:09 +0200

    new commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import { HiOutlineSearchCircle } from "react-icons/hi";
 
 function Header() {
   return (
@@ -7,8 +8,95 @@ function Header() {
       <header className="header-top-strip py-3">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-6"><p>Free shipping worldwide</p></div>
-            <div className="col-6"><p className="text-end">  Hotline: <a href="tel:+49 0101010101">+49 0101010101</a> </p></div>
+            <div className="col-6">
+              <p className="text-white">Free shipping worldwide</p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                {" "}
+                Hotline:{" "}
+                <a className="text-white" href="tel:+49 0101010101">
+                  +49 0101010101
+                </a>{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h2>
+                {" "}
+                <Link className=" text-white">Logo</Link>
+              </h2>
+            </div>
+            <div className="col-5">
+              <div class="input-group">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Search Product here"
+                  aria-label="Search Product here"
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-23" id="basic-addon2">
+                  <HiOutlineSearchCircle className="fs-4" />
+                </span>
+              </div>{" "}
+            </div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="cart-container d-flex align-items-center text-white">
+                    <img src="images/compare.svg" alt="compare" />
+                    <p className="mb-0">
+                      compare <br />
+                      Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="cart-container d-flex align-items-center text-white">
+                    <img src="images/wishlist.svg" alt="wishlist" />
+                    <p className="mb-0">
+                      Favorite <br />
+                      WishList
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="cart-container d-flex align-items-center text-white">
+                    <img src="images/user.svg" alt="user" />
+                    <p className="mb-0">
+                      Login <br />
+                      My Account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="cart-container d-flex align-items-center text-white">
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className="d-flex flex-column gap-2">
+                      <span className="badge bg-white text-dark mb-0">0</span>
+                      <span className="mb-0">$ 200</span>
+                    </div>
+                  </Link>
+                </div>
+              </div>{" "}
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center">
+
+              </div>
+            </div>
           </div>
         </div>
       </header>
